a83e148b6a9c242d079bd10335ac34400acc40ce
Manojlodhi9691 <manojlodhi9691@gmail.com>
2026-02-23 01:32:20 +0530

    h

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
